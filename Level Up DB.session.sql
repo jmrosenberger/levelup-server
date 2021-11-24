39801607f8c601ea394d447e25ae50c417323dba
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-11-24 15:12:04 -0600

    completed chapter 13, user can now access profile info

--- Level Up DB.session.sql
+++ Level Up DB.session.sql
@@ -1,7 +1,15 @@
-SELECT E.event_id AS Event, COUNT(gamer_id) AS Attendee_Count, A.username AS Username
+SELECT E.event_id AS Name_Of_Event, 
+    Ga.Title AS Game_Title, 
+    Ev.Description AS About,
+    COUNT(E.gamer_id) AS Attendee_Count, 
+    A.username AS Username, A.email AS Email
 FROM levelupapi_eventgamer AS E
 JOIN levelupapi_gamer AS G
-ON E.gamer_id = G.id
+    ON E.gamer_id = G.id
 JOIN auth_user AS A
-ON A.id = G.user_id
--- GROUP BY e.event_id
+    ON A.id = G.user_id
+JOIN levelupapi_event AS Ev
+    ON Ev.id = E.Event_id
+JOIN levelupapi_game AS Ga
+    ON Ev.game_id = Ga.id
+GROUP BY e.event_id
