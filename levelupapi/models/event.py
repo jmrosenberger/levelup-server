1567008156ebef95656db0f3a053e8b8754610c4
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-11-29 02:01:22 -0600

    completed supp ch.1, game list shows event count

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Event(models.Model):
-    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name='events')
     description = models.CharField(max_length=100)
-    date = models.DateField()
+    date = models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField()
     organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="attending")
--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from levelupapi.models import Game, GameType, Gamer
+from django.db.models import Count
 
 
 class GameView(ViewSet):
@@ -123,7 +124,8 @@ class GameView(ViewSet):
         """
         # Get all game records from the database
         gamer = Gamer.objects.get(user=request.auth.user)
-        games = Game.objects.all()
+        games = Game.objects.annotate(event_count=Count('events'))
+
 
         # Support filtering games by type
         #    http://localhost:8000/games?type=1
@@ -144,7 +146,8 @@ class GameSerializer(serializers.ModelSerializer):
     Arguments:
         serializer type
     """
+    event_count = serializers.IntegerField(default=None)
     class Meta:
         model = Game
-        fields = ('id', 'title', 'maker', 'number_of_players', 'skill_level', 'game_type', 'gamer')
+        fields = ('id', 'title', 'maker', 'number_of_players', 'skill_level', 'game_type', 'gamer', 'event_count')
         depth = 1
